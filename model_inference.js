07f399cc7061b3948439eb20d17c377dffb98133
a-regal <andresregal97@gmail.com>
2020-07-05 18:50:08 -0500

    Slight changes

--- model_inference.js
+++ model_inference.js
@@ -1,7 +1,8 @@
 //import { Tensor, InferenceSession } from "onnxjs";
 
 async function load_model() {
-  const session = new onnx.InferenceSession();
+  const session = new onnx.InferenceSession({ backendHint: 'webgl' });
+  //const url = "./actor_critic.onnx";
   const url = "input_params/actor_critic.onnx";
   await session.loadModel(url);
   console.log('Model loaded');
