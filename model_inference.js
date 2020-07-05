11e715b5fbfacf6ddb177719db9e09ddb96cec94
a-regal <andresregal97@gmail.com>
2020-07-05 18:56:38 -0500

    add shapefile read

--- model_inference.js
+++ model_inference.js
@@ -8,4 +8,16 @@ async function load_model() {
   console.log('Model loaded');
 }
 
+async function load_network() {
+  await shapefile.open("./input_params/downtown_network/downtown_network.shp")
+    .then(source => source.read()
+      .then(function log(result) {
+        if (result.done) return;
+        console.log(result.value);
+        return source.read().then(log);
+      }))
+    .catch(error => console.error(error.stack));
+}
+
 load_model()
+load_network()
